07b94b401ee652de498f5c602eee34ab633889c1
qas <qassoumohamed@gmail.com>
2017-02-11 14:52:47 +0100

    modif fichier Database.js

--- efrei_backend/models/database.js
+++ efrei_backend/models/database.js
@@ -1,10 +1,23 @@
-var pgp= require('pg-promise')();
-var config={
-host:'localhost',
-port:5432,
-database:'efrei',
-user:'efrei',
-password:'password'
+var pgp = require('pg-promise')();
+var path = require('path');
+var QueryFile = require('pg-promise').QueryFile;
+
+var config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'efrei',
+  user: 'efrei',
+  password: 'password'
 };
-const DB =pgp(config);
-module.exports=DB;
+
+const DB = {
+  accessor: pgp(config),
+
+  sql: function (file) {
+    var fullPath = path.join(__dirname, file); // generating full path;
+    return new QueryFile(fullPath, { minify: false });
+  }
+
+};
+
+module.exports = DB;
