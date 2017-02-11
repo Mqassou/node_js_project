3f174682bfa0be4ea329d74f812ca97f63fba5ed
qas <qassoumohamed@gmail.com>
2017-02-11 22:17:30 +0100

    routes pour characters

---
+++ efrei_backend/models/characterdao.js
@@ -0,0 +1,85 @@
+const DB =require('../models/Database');
+
+module.exports={
+getById(id){
+
+return DB.accessor.query('SELECT * FROM characters WHERE id=$[idCharacter]', {
+    idCharacter: id
+	})
+	.then((result)=>
+	{
+	
+	return result
+	})
+	.catch((error)=>{
+	throw error
+	})
+	
+	
+},
+getAll(){
+return DB.accessor.query('SELECT * FROM characters')
+	.then((result)=>{
+	return result
+	})
+	.catch((error)=>{
+	throw error
+	})
+
+
+},
+
+insertCharacter(name,idUser,_class,positionX,positionY)
+{
+return DB.accessor.query('INSERT INTO characters (name,user_id,class,position) VALUES(${character_name},${user_id},${character_class},point(${character_positionX},${character_positionY})) RETURNING *', {
+	character_name:name,
+	user_id:idUser,
+    character_class: _class,
+	character_positionX:positionX,
+	character_positionY:positionY
+	})
+	.then((result)=>{
+	result=result[0];// pour enlever les crochets
+	return result
+	})
+	.catch((error)=>{
+	throw error
+	})
+	
+
+},
+
+updateCharacter(id,name,idUser,_class,positionX,positionY)
+{
+return DB.accessor.query('UPDATE  characters SET name=${character_name}, user_id=${user_id},class=${character_class},position=point(${character_positionX},${character_positionY}) WHERE id =${character_id} RETURNING *', {
+	character_id:id,
+	character_name:name,
+	user_id:idUser,
+    character_class: _class,
+	character_positionX:positionX,
+	character_positionY:positionY
+	})
+	.then((result)=>{
+	result=result[0];// pour enlever les crochets
+	return result
+	})
+	.catch((error)=>{
+	throw error
+	})
+	
+
+},
+deleteCharacter(id)
+{
+return DB.accessor.query('DELETE FROM characters WHERE id =${character_id} ', {
+	character_id:id
+
+	})
+	.catch((error)=>{
+	throw error
+	})
+	
+
+}
+}
+
---
+++ efrei_backend/routes/characters.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var CharacterDAO=require('../models/characterdao');
+var router = express.Router();
+
+
+router.get('/',function(req,res,next){
+var id=parseInt(req.params.id);
+CharacterDAO.getAll()
+.then((characters)=>{
+res.status(200).send({ status: 'success', characters: characters });
+})
+.catch((error)=>
+	res.status(500)
+		.json({
+			status:'Error',
+			message:error
+			}));
+});
+
+
+router.get('/:id',function(req,res,next){
+var id=parseInt(req.params.id);
+CharacterDAO.getById(id)
+.then((character)=>{
+res.status(200).send({ status: 'success', character: character });
+})
+.catch((error)=>
+	res.status(500)
+		.json({
+			status:'Error',
+			message:error
+			}));
+});
+
+
+
+router.post('/', function(req, res) {
+    var character_name = req.body.character.name;
+    var user_id = req.body.character.user_id;
+    var character_class = req.body.character.class;
+    var character_positionX = req.body.character.position.x;
+	var character_positionY = req.body.character.position.y;
+
+CharacterDAO.insertCharacter(character_name,user_id,character_class,character_positionX,character_positionY)
+.then((character)=>{
+
+res.status(200).send({ status: 'success',message:'Inserted one character', character: character });
+})
+.catch((error)=>
+	res.status(500)
+		.json({
+			status:'Error',
+			message:error
+			}));
+
+});
+
+
+router.put('/:id', function(req, res) {
+	var character_id=parseInt(req.params.id);
+    var character_name = req.body.character.name;
+    var user_id = req.body.character.user_id;
+    var character_class = req.body.character.class;
+    var character_positionX = req.body.character.position.x;
+	var character_positionY = req.body.character.position.y;
+
+CharacterDAO.updateCharacter(character_id,character_name,user_id,character_class,character_positionX,character_positionY)
+.then((character)=>{
+
+res.status(200).send({ status: 'success',message:'modified a character', character: character });
+})
+.catch((error)=>
+	res.status(500)
+		.json({
+			status:'Error',
+			message:error
+			}));
+
+});
+
+router.delete('/:id', function(req, res) {
+	var character_id=parseInt(req.params.id);
+
+CharacterDAO.deleteCharacter(character_id)
+.then((character)=>{
+
+res.status(200).send({ status: 'success',message:'deleted a character' });
+})
+.catch((error)=>
+	res.status(500)
+		.json({
+			status:'Error',
+			message:error
+			}));
+
+});
+
+module.exports = router;
